9d83789ae31d1e758e96c9c6d46730287aa3fdff
mosc5 <moritz.schiel@rl-institut.de>
2022-04-28 08:52:47 +0200

    initial test setup

--- tests/test_something.py
+++ tests/test_something.py
@@ -6,7 +6,7 @@ https://github.com/rl-institut/workshop/tree/master/test-driven-development.
 Otherwise https://docs.pytest.org/en/latest/ and https://docs.python.org/3/library/unittest.html
 are also good support.
 """
-import pytest
+import src.simulation
 
 
 # this function will not run as a test as its name does not start by "test_"
@@ -18,9 +18,3 @@ def addition(a, b):
 # something has to be asserted within the function
 def test_addition():
     assert addition(2, 2) == 4
-
-
-# one can test that exception are raised
-def test_addition_wrong_argument_number():
-    with pytest.raises(TypeError):
-        assert addition(2) == 2  # pylint: disable=E1120
