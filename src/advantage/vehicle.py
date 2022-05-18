0ddccfa0c07e736a92118848df158a6768b0bdd9
yplatow <yannick.platow@rl-institut.de>
2022-05-18 13:29:00 +0200

    added functions charge, drive,...

--- src/advantage/vehicle.py
+++ src/advantage/vehicle.py
@@ -26,20 +26,67 @@ class Vehicle:
         self.output = None
         self.task = None
 
+        self.output = {
+            "timestamp": [],
+            "event_start": [],
+            "event_time": [],
+            "location": [],
+            "use_case": [],
+            "soc": [],
+            "charging_demand": [],
+            "nominal_charging_capacity": [],  # rethink these?
+            "charging_power": [],
+            "consumption": []
+        }
+
+    def _update_activity(self, timestamp, event_start, event_time,
+                         nominal_charging_capacity=0, charging_power=0):
+        """Records newest energy and activity"""
+        self.soc = round(self.soc, 4)
+        self.output["timestamp"].append(timestamp)
+        self.output["event_start"].append(event_start)
+        self.output["event_time"].append(event_time)
+        self.output["location"].append(self.status)
+        # self.output["use_case"].append(self._get_usecase())
+        self.output["soc"].append(self.soc)
+        self.output["charging_demand"].append(self._get_last_charging_demand())
+        self.output["nominal_charging_capacity"].append(nominal_charging_capacity)
+        self.output["charging_power"].append(charging_power)
+        self.output["consumption"].append(self._get_last_consumption())
+
     def charge(self, trip, power, charging_type):
         # call spiceev charging depending on soc, location, task
         self.status = 'charging'
         usable_power = min(power, self.vehicle_type.charging_capacity[charging_type])
-        self.soc = min(self.soc + trip.park_time * usable_power / self.car_type.battery_capacity, 1)
-        # self._update_activity(trip.park_timestamp, trip.park_start, trip.park_time,
-                              # nominal_charging_capacity=power, charging_power=usable_power)
+        self.soc = min(self.soc + trip.park_time * usable_power / self.vehicle_type.battery_capacity, 1)
+        self._update_activity(trip.park_timestamp, trip.park_start, trip.park_time,
+                              nominal_charging_capacity=power, charging_power=usable_power)
         return
 
     def drive(self, trip):
         # call drive api with task, soc, ...
         self.status = 'driving'
-        self.soc -= self.car_type.consumption * trip.distance / self.car_type.battery_capacity
+        self.soc -= self.vehicle_type.consumption * trip.distance / self.vehicle_type.battery_capacity
+        self._update_activity(trip.drive_timestamp, trip.drive_start, trip.drive_time)
+        self.status = trip.destination
+
         return
+
+    def _get_last_charging_demand(self):
+        if len(self.output["soc"]) > 1:
+            charging_demand = (self.output["soc"][-1] - self.output["soc"][-2])
+            charging_demand *= self.vehicle_type.battery_capacity
+            return max(round(charging_demand, 4), 0)
+        else:
+            return 0
+
+    def _get_last_consumption(self):
+        if len(self.output["soc"]) > 1:
+            last_consumption = self.output["soc"][-1] - self.output["soc"][-2]
+            last_consumption *= self.vehicle_type.battery_capacity
+            return abs(min(round(last_consumption, 4), 0))
+        else:
+            return 0
 
 
 @dataclass
@@ -54,7 +101,7 @@ class VehicleType:
     min_charging_power: least amount of charging power possible, as a share of max power
     """
     name: str
-    capacity: float
+    battery_capacity: float
     base_consumption: float
     charging_curve: list
     min_charging_power: float
