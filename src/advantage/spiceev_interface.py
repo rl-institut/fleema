69eed782d1aebf6199102d20a1a87dbc0b4600b0
mosc5 <moritz.schiel@rl-institut.de>
2022-06-29 16:42:54 +0200

    save dates in iso format #12

--- src/advantage/spiceev_interface.py
+++ src/advantage/spiceev_interface.py
@@ -4,16 +4,17 @@ import datetime
 def get_spice_ev_scenario_dict(vehicle, location, timestamp, time):
     scenario_dict = {
         "scenario": {
-            "start_time": "2018-01-01T01:00:00+02:00",
-            "interval": 15,
-            "n_intervals": 2880,
+            "start_time": timestamp.isoformat(),
+            "interval": 1,
+            "n_intervals": time,
             "discharge_limit": 0.5
         },
         "constants": {}
     }
     spice_ev_dict = dict(scenario_dict, **vehicle.scenario_info)
     # spice_ev_dict.update(location.scenario_info)  # TODO implement
+    departure_time = timestamp + datetime.timedelta(minutes=time)
     spice_ev_dict["constants"]["vehicles"][f"{vehicle.vehicle_type.name}_0"]["estimated_time_of_departure"] = \
-        str(timestamp + datetime.timedelta(minutes=time))
+        departure_time.isoformat()
 
     return spice_ev_dict
