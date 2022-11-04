772a7d738f7f6253da6864e0ab3e734f65818bea
mosc5 <moritz.schiel@gmx.net>
2022-11-04 10:53:52 +0100

    restructure sim state tests #26

--- tests/test_simulation_state.py
+++ tests/test_simulation_state.py
@@ -26,18 +26,18 @@ def sim_state():
     return SimulationState()
 
 
-def test_constructor(sim_state: "SimulationState"):
+def test_constructor(sim_state):
     assert sim_state.charging_vehicles == []
 
 
-def test_drive_result(car, time_series, sim_state: "SimulationState"):
+def test_drive_result(car, time_series, sim_state):
     start_step = 5
     time_stamp = step_to_timestamp(time_series, start_step)
     car.drive(time_stamp, start_step, 10, "station_1", 0.45, sim_state)
     assert sim_state.driving_vehicles[0] == car
 
 
-def test_multi_drive(car, time_series, sim_state: "SimulationState"):
+def test_multi_drive(car, time_series, sim_state):
     for i in range(1, 5):
         start_step = 5 * i
         time_stamp = step_to_timestamp(time_series, start_step)
@@ -45,37 +45,45 @@ def test_multi_drive(car, time_series, sim_state: "SimulationState"):
     assert len(sim_state.driving_vehicles) == 1
 
 
-def test_remove_vehicle_from_driving(car, sim_state: "SimulationState"):
+def test_remove_vehicle(car, sim_state):
     sim_state.driving_vehicles.append(car)
+    sim_state.parking_vehicles.append(car)
+    sim_state.charging_vehicles.append(car)
+    sim_state.remove_vehicle(car)
+    error_list = []
+    if car in sim_state.driving_vehicles:
+        error_list.append("Vehicle still in driving list")
+    if car in sim_state.parking_vehicles:
+        error_list.append("Vehicle still in parking list")
+    if car in sim_state.charging_vehicles:
+        error_list.append("Vehicle still in charging list")
+
+    assert not error_list, "errors occured:\n{}".format("\n".join(error_list))
+
+
+def test_remove_multi_vehicle(car, sim_state):
+    for i in range(5):
+        sim_state.driving_vehicles.append(car)
     sim_state.remove_vehicle(car)
     assert car not in sim_state.driving_vehicles
 
 
-def test_remove_vehicle_from_parking(car, sim_state: "SimulationState"):
-    sim_state.parking_vehicles.append(car)
-    sim_state.remove_vehicle(car)
-    assert car not in sim_state.parking_vehicles
-
-
-def test_remove_vehicle_from_charging(car, sim_state: "SimulationState"):
-    sim_state.charging_vehicles.append(car)
-    sim_state.remove_vehicle(car)
-    assert car not in sim_state.charging_vehicles
-
-
-def test_update_vehicle_drive(car, sim_state: "SimulationState"):
+def test_update_vehicle_drive(car, sim_state):
+    car.status = "driving"
     sim_state.update_vehicle(car)
-    # TODO car is updated into right list
+    assert car in sim_state.driving_vehicles
 
 
-def test_update_vehicle_charge(car, sim_state: "SimulationState"):
+def test_update_vehicle_charge(car, sim_state):
+    car.status = "charging"
     sim_state.update_vehicle(car)
-    # TODO car is updated into right list
+    assert car in sim_state.charging_vehicles
 
 
-def test_update_vehicle_park(car, sim_state: "SimulationState"):
+def test_update_vehicle_park(car, sim_state):
+    car.status = "parking"
     sim_state.update_vehicle(car)
-    # TODO car is updated into right list
+    assert car in sim_state.parking_vehicles
 
 
 '''
