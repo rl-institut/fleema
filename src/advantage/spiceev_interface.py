c754969cb3992d45e4c1dba58bcbe146a06c007f
lilAndy-bruh <alexkens93@gmail.com>
2022-11-07 14:59:24 +0100

    Add docstrings in spiceev_interface.py #20

--- src/advantage/spiceev_interface.py
+++ src/advantage/spiceev_interface.py
@@ -5,6 +5,27 @@ from advantage.util.helpers import deep_update
 
 
 def get_spice_ev_scenario_dict(vehicle, location, point_id, timestamp, time):
+    """This function creates a dictionary for SpiceEV.
+
+    Parameters
+    ----------
+    vehicle : Vehicle
+        Given Vehicle object for this SpiceEV scenario.
+    location : Location
+        Used location.
+    point_id : int
+        ID of the ChargingPoint.
+    timestamp : str
+        Timestamp that marks the starting time.
+    time : int
+        Number of time intervals.
+
+    Returns
+    -------
+    dict
+        Nested SpiceEV dictionary.
+
+    """
     scenario_dict = {
         "scenario": {
             "start_time": timestamp.isoformat(),
@@ -30,6 +51,21 @@ def get_spice_ev_scenario_dict(vehicle, location, point_id, timestamp, time):
 
 
 def run_spice_ev(spice_ev_dict, strategy) -> "Scenario":
+    """This function runs the scenario and returns it.
+
+    Parameters
+    ----------
+    spice_ev_dict : dict
+        Nested SpiceEV dictionary that holds all relevant information for the scenario.
+    strategy : str
+        Name of the strategy.
+
+    Returns
+    -------
+    Scenario
+        Scenario object.
+
+    """
     scenario = Scenario(spice_ev_dict)
     scenario.run(strategy, {})
     return scenario
