29cb57fb30aecc43cc23d0810bb97b4c62f3679d
lilAndy-bruh <alexkens93@gmail.com>
2023-02-27 21:20:09 +0100

    Make flake8 happy #56

--- src/advantage/simulation_types/schedule.py
+++ src/advantage/simulation_types/schedule.py
@@ -1,7 +1,3 @@
-import pathlib
-
-import pandas as pd
-
 from advantage.simulation_type import SimulationType
 from typing import TYPE_CHECKING
 from operator import itemgetter
@@ -262,6 +258,6 @@ class Schedule(SimulationType):
         if self.simulation.outputs["vehicle_csv"]:
             self.simulation.observer.export_log(self.simulation.save_directory)
 
-        # generate power prid timeseries for locations
+        # generate power grid timeseries for locations
         for location in self.simulation.charging_locations:
             location.export(self.simulation.time_series, self.simulation.save_directory)
