4581c79968972ed51cb3f400e1d5aa5c84f46171
mosc5 <moritz.schiel@rl-institut.de>
2023-06-05 10:42:52 +0200

    actually pull load_level_default from config

--- src/advantage/util/helpers.py
+++ src/advantage/util/helpers.py
@@ -95,7 +95,7 @@ def read_input_data(scenario_data_path, cfg):
     if "level_of_loading" not in data_dict["schedule"].keys():
         if "occupation" not in data_dict["schedule"].keys():
             level_of_loading = [
-                cfg.getfloat("sim_params", "load_level_default", fallback=0.)
+                cfg.getfloat("defaults", "load_level_default", fallback=0.)
                 for _ in range(data_dict["schedule"].shape[0])
             ]
         else:
