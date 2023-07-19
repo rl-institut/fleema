ce9f08f9e815fff2ad24846d887a6302cbc502d9
lilAndy-bruh <alexkens93@gmail.com>
2023-07-19 12:31:37 +0200

    fix characteristic wrapper, improve tests #95

--- tests/test_spiceev_interface.py
+++ tests/test_spiceev_interface.py
@@ -82,7 +82,7 @@ def test_run_spice_ev(car, time_series, spot, cost_options):
 
 @pytest.fixture()
 # scenario with step size 1 and interval 1
-def scenario_1(car, spot, time_series, cost_options):
+def scenario_n_intervals_1(car, spot, time_series, cost_options):
     spice_dict = get_spice_ev_scenario_dict(
         car, spot, "point_0", step_to_timestamp(time_series, 5), 1, cost_options, 1
     )
@@ -94,7 +94,7 @@ def scenario_1(car, spot, time_series, cost_options):
 
 @pytest.fixture()
 # scenario with step size 1 and interval 15
-def scenario_2(car, spot, time_series, cost_options):
+def scenario_n_intervals_15(car, spot, time_series, cost_options):
     spice_dict = get_spice_ev_scenario_dict(
         car, spot, "point_0", step_to_timestamp(time_series, 5), 15, cost_options, 1
     )
@@ -106,7 +106,7 @@ def scenario_2(car, spot, time_series, cost_options):
 
 @pytest.fixture()
 # scenario with step size 15 and interval 1
-def scenario_3(car, spot, time_series, cost_options):
+def scenario_step_size_15(car, spot, time_series, cost_options):
     spice_dict = get_spice_ev_scenario_dict(
         car, spot, "point_0", step_to_timestamp(time_series, 5), 1, cost_options, 15
     )
@@ -116,26 +116,42 @@ def scenario_3(car, spot, time_series, cost_options):
     return run_spice_ev(spice_dict, "balanced")
 
 
-def test_get_charging_characteristic_different_intervals(scenario_1, scenario_2):
+def test_get_charging_characteristic_same_step_size_different_intervals(
+        scenario_n_intervals_1, scenario_n_intervals_15):
+    charging_result_1 = get_charging_characteristic((scenario_n_intervals_1, None), 0.05)
+    charging_result_2 = get_charging_characteristic((scenario_n_intervals_1, scenario_n_intervals_1), 0.05)
+    charging_result_3 = get_charging_characteristic((scenario_n_intervals_15, None), 0.05)
+    charging_result_4 = get_charging_characteristic((scenario_n_intervals_15, scenario_n_intervals_15), 0.05)
+    charging_result_5 = get_charging_characteristic((scenario_n_intervals_1, scenario_n_intervals_15), 0.05)
 
-    charging_result_1 = get_charging_characteristic((scenario_1, None), 0.05)
-    charging_result_2 = get_charging_characteristic((scenario_1, scenario_1), 0.05)
-    charging_result_3 = get_charging_characteristic((scenario_2, None), 0.05)
-    charging_result_4 = get_charging_characteristic((scenario_2, scenario_2), 0.05)
-    charging_result_5 = get_charging_characteristic((scenario_1, scenario_2), 0.05)
+    assert charging_result_1["cost"] * 2 == charging_result_2["cost"]
+    assert charging_result_1["emission"] * 2 == charging_result_2["emission"]
+    assert charging_result_1["feed_in"] == charging_result_2["feed_in"]
+    assert charging_result_1["grid_energy"] * 2 == charging_result_2["grid_energy"]
 
-    assert [i * 2 for i in charging_result_1.values()] == list(charging_result_2.values())
-    assert [i * 2 for i in charging_result_3.values()] == list(charging_result_4.values())
-    assert [i * scenario_2.n_intervals for i in charging_result_1.values()] == list(charging_result_3.values())
-    assert [i + j for i, j in zip(charging_result_1.values(), charging_result_3.values())] \
-           == list(charging_result_5.values())
+    assert charging_result_3["cost"] * 2 == charging_result_4["cost"]
+    assert charging_result_3["emission"] * 2 == charging_result_4["emission"]
+    assert charging_result_3["feed_in"] == charging_result_4["feed_in"]
+    assert charging_result_3["grid_energy"] * 2 == charging_result_4["grid_energy"]
+
+    assert charging_result_5["cost"] == charging_result_1["cost"] + charging_result_3["cost"]
+    assert charging_result_5["emission"] == charging_result_1["emission"] + charging_result_3["emission"]
+    assert charging_result_5["feed_in"] * charging_result_5["grid_energy"] * 60 == \
+           charging_result_1["feed_in"] * charging_result_1["grid_energy"] * scenario_n_intervals_1.stepsPerHour\
+           + charging_result_3["feed_in"] * charging_result_3["grid_energy"] * scenario_n_intervals_15.stepsPerHour
+    assert charging_result_5["grid_energy"] == charging_result_1["grid_energy"] + charging_result_3["grid_energy"]
 
 
-def test_get_charging_characteristic_different_step_size(scenario_1, scenario_3):
+def test_get_charging_characteristic_different_step_size_same_intervals(scenario_n_intervals_1, scenario_step_size_15):
+    charging_result_1 = get_charging_characteristic((scenario_n_intervals_1, None), 0.05)
+    charging_result_2 = get_charging_characteristic((scenario_step_size_15, None), 0.05)
+    charging_result_3 = get_charging_characteristic((scenario_n_intervals_1, scenario_step_size_15), 0.05)
 
-    charging_result_1 = get_charging_characteristic((scenario_1, None), 0.05)
-    charging_result_2 = get_charging_characteristic((scenario_3, None), 0.05)
-    charging_result_3 = get_charging_characteristic((scenario_1, scenario_3), 0.05)
-
-    assert [i + j for i, j in zip(charging_result_1.values(), charging_result_2.values())] \
-           == list(charging_result_3.values())
+    assert charging_result_3["cost"] == charging_result_1["cost"] + charging_result_2["cost"]
+    assert charging_result_3["emission"] == charging_result_1["emission"] + charging_result_2["emission"]
+    assert charging_result_3["feed_in"] * charging_result_3["grid_energy"] * 60 == \
+           charging_result_1["feed_in"] * charging_result_1["grid_energy"] * scenario_n_intervals_1.stepsPerHour\
+           + charging_result_2["feed_in"] * charging_result_2["grid_energy"] * scenario_step_size_15.stepsPerHour
+    assert charging_result_3["grid_energy"] == charging_result_1["grid_energy"] * \
+           scenario_n_intervals_1.stepsPerHour / 60\
+           + charging_result_2["grid_energy"] * scenario_step_size_15.stepsPerHour / 60
