c7e83bcd47f027dab1b9a7ba09909bfab8d41886
lilAndy-bruh <alexkens93@gmail.com>
2023-07-26 11:16:49 +0200

    Resolve review requests no2 #98

--- src/advantage/simulation_type.py
+++ src/advantage/simulation_type.py
@@ -109,10 +109,11 @@ class SimulationType:
                 for _ in range(int(spiceev_scenarios[0].interval.total_seconds() / 60))
             ]
             if spiceev_scenarios[1]:
-                charging_power_list_remainder = []
-                for charge_value in spiceev_scenarios[1].connChargeByTS["GC1"]:
-                    charging_power_list_remainder.append(list(charge_value.values())[0])
-                charging_power_list += charging_power_list_remainder
+                # second scenario has always same stepsize as simulation which is 1
+                charging_power_list_remainder = [
+                    list(charge_value.values())[0] for charge_value in spiceev_scenarios[1].connChargeByTS["GC1"]
+                ]
+                charging_power_list.extend(charging_power_list_remainder)
 
             average_charging_power = sum(charging_power_list) / len(charging_power_list)
             # execute charging event
