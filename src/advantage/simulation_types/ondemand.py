d483740ff9a0a8c1162fcee995941da340358366
lilAndy-bruh <alexkens93@gmail.com>
2023-11-23 14:37:57 +0100

    Finish reschedule(), add init_schedule() #107

--- src/advantage/simulation_types/ondemand.py
+++ src/advantage/simulation_types/ondemand.py
@@ -17,8 +17,10 @@ class Ondemand(SimulationType):
         self.new_tasks = []
 
     def run(self):
+        """Run the scenario with this strategy."""
         # create tasks for all vehicles from input schedule
         self.simulation.vehicles_from_schedule()
+        self.init_schedule()
 
         # create save directory
         if True in self.simulation.outputs.values():
@@ -51,12 +53,14 @@ class Ondemand(SimulationType):
         filtered_df = filtered_df[filtered_df['departure_time'] <= threshold_time]
         filtered_df['departure_time'] = filtered_df['departure_time'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
+        # if no new driving tasks arrived, the schedule can remain the same
         if filtered_df.empty:
             # start 0 could have no driving tasks, so first it has to find charging events
             if start == 0:
                 self.reschedule(start, window)
             return
 
+        # if new driving tasks arrived, charging events have to be rescheduled
         filtered_df.apply(self.simulation.task_from_schedule, axis=1)
         filtered_df.apply(self.get_task, axis=1)
         self.reschedule(start, window)
@@ -67,18 +71,28 @@ class Ondemand(SimulationType):
         for veh in self.simulation.vehicles.values():
             break_list = veh.get_breaks(start, window)
             score_list = []
-            # evaluate_charging_location -> every break
+
+            # evaluate_charging_location -> every break that is at a charging_location, otherwise add parking_event
             for break_event in break_list:
-                score = self.simulation.evaluate_charging_location(
-                    veh.vehicle_type,
-                    break_event.start_point,
-                    veh.current_location,
-                    break_event.start_point,
-                    break_event.start_time,
-                    break_event.end_time,
-                    veh.soc,
-                )
-                score_list.append(score)
+                if break_event.start_point in self.simulation.charging_locations:
+                    score = self.simulation.evaluate_charging_location(
+                        veh.vehicle_type,
+                        break_event.start_point,
+                        break_event.start_point,
+                        break_event.start_point,
+                        break_event.start_time,
+                        break_event.end_time,
+                        veh.soc,
+                    )
+                    score_list.append(score)
+                else:
+                    veh.add_task(Task(
+                        break_event.start_time,
+                        break_event.end_time,
+                        break_event.start_point,
+                        break_event.start_point,
+                        Status.PARKING,
+                    ))
             score_list.sort(key=itemgetter("score"), reverse=True)
             # choose the best ones according to eval-score and soc_min
             while self.get_predicted_soc(veh, start, window) < self.simulation.soc_min:
@@ -86,6 +100,40 @@ class Ondemand(SimulationType):
 
     def get_task(self, row):
         self.new_tasks.append((row["departure_time"], row["arrival_time"]))
+
+    def init_schedule(self):
+        # TODO: break_list is only one element?
+        for veh in self.simulation.vehicles.values():
+            break_list = veh.get_breaks(0, self.horizon)
+            score_list = []
+
+            # look at schedule to get location from first driving event and check if it is a charging location
+            init_location_name = self.filtered_schedule.loc[self.filtered_schedule["vehicle_id"] == veh.id]["departure_name"].values[0]
+            init_location = self.simulation.locations[init_location_name]
+            if init_location in self.simulation.charging_locations:
+                # evaluate_charging_location -> every break
+                for break_event in break_list:
+                    score = self.simulation.evaluate_charging_location(
+                        veh.vehicle_type,
+                        init_location,
+                        break_event.start_time,
+                        break_event.end_time,
+                        veh.soc,
+                    )
+                    score_list.append(score)
+                score_list.sort(key=itemgetter("score"), reverse=True)
+                # choose the best ones according to eval-score and soc_min
+                while self.get_predicted_soc(veh, 0, self.horizon) < self.simulation.soc_min:
+                    veh.add_task(score_list.pop()["charge_event"])
+            else:
+                for break_event in break_list:
+                    veh.add_task(Task(
+                        break_event.start_time,
+                        break_event.end_time,
+                        init_location,
+                        init_location,
+                        Status.PARKING,
+                    ))
 
 
 def greedy_charging_schedule(electricity_cost, charging_time, charging_point_capacity, start_soc, end_of_day_soc):
